07bd7edfb140d9bf206239965aff74027c83d5a1
alto <victor1301166040@gmail.com>
2021-09-07 21:24:19 -0700

    +

--- Python/Populating_Next_Right_Pointer_in_Each_Node/solution.py
+++ Python/Populating_Next_Right_Pointer_in_Each_Node/solution.py
@@ -1,1 +1,35 @@
-# TODO: Fill this up plz. 
+from typing import * 
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        # Edge case 
+        if root is None or root.left is None: 
+            return root 
+        # Base case, construct the first layer correct
+        root.left.next = root.right
+        # Recursive case, use the previous later to link the next layer. 
+        Parent = root.left
+        LeftMost = Parent # left most of the previous layer. 
+        while LeftMost.left is not None:
+            while Parent is not None: 
+                Left = Parent.left 
+                Right = Parent.right
+                Left.next = Right
+                if Parent.next is not None:
+                    Right.next = Parent.next.left
+                Parent = Parent.next
+            LeftMost = LeftMost.left 
+            Parent = LeftMost
+        return root
+
+
